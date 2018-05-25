FIX normalization and plotting

segmentation/dataloader.py
@@ -114,15 +114,18 @@ def load_patches(subject, random_state=None):
 
     # Load one subject
     t1w_im, labels_im = load_brain(subject)
+    t1w_im = np.asarray(t1w_im, dtype=np.float32)
 
     shape = np.asarray(t1w_im.shape)
     shape -= 64 + 1
     i0 = [rng.randint(m) for m in shape]
     w0, h0, z0 = i0
 
-    X = torch.from_numpy(t1w_im[w0:w0 + 64,
-                                h0:h0 + 64,
-                                z0:z0 + 64].reshape((1, 1, 64, 64, 64)))
+    X = t1w_im[w0:w0 + 64,
+               h0:h0 + 64,
+               z0:z0 + 64].reshape((1, 1, 64, 64, 64))
+    X /= np.max(t1w_im)
+    X = torch.from_numpy(X)
 
     y = torch.from_numpy(labels_im[w0:w0 + 64,
                                    h0:h0 + 64,

training_unet.py
@@ -6,8 +6,8 @@ import torch.multiprocessing as mp
 
 
 from segmentation.unet import Unet, segmentation_loss
-from segmentation.dataloader import load_brain, get_queue_feeder
-from segmentation.dataloader import cut_image, stitch_image
+from segmentation.dataloader import load_brain, get_queue_feeder, load_patches
+from segmentation.dataloader import cut_image, stitch_image, _EXAMPLE_SUBJECT
 from segmentation.plotting import plot_segmentation, plot_patch_prediction
 
 
@@ -24,20 +24,22 @@ if __name__ == "__main__":
                         help='# of process to load the patches.')
 
     args = parser.parse_args()
+    print(args.gpu)
 
     from segmentation.config import DATA_DIR
-    DATA_DIR_PATH = pathlib.Path(DATA_DIR)
-    tst_subject = list(DATA_DIR_PATH.glob('[0-9]*/'))[0]
+    # DATA_DIR_PATH = pathlib.Path(DATA_DIR)
+    # tst_subject = list(DATA_DIR_PATH.glob('[0-9]*/'))[0]
 
-    queue_feed, stop_event, batch_feeder = get_queue_feeder(
-        batch_size=1, maxsize_queue=20, n_process=args.preprocessors)
+    tst_subject = _EXAMPLE_SUBJECT
+    # queue_feed, stop_event, batch_feeder = get_queue_feeder(
+    #     batch_size=1, maxsize_queue=20, n_process=args.preprocessors)
 
     unet = Unet(n_outputs=4)
     if args.gpu:
         torch.cuda.set_device(0)
         unet = unet.cuda()
 
-    learning_rate = 1e-10
+    learning_rate = 1e-3
     optimizer = torch.optim.Adam(unet.parameters(), lr=learning_rate)
 
     X_tst, y_tst = load_brain(tst_subject)
@@ -45,16 +47,18 @@ if __name__ == "__main__":
     batch_tst, labels_tst = cut_image(X_tst), cut_image(y_tst)
     batch_tst = np.array(list(batch_tst), dtype=np.float32)
     labels_tst = np.array(list(labels_tst), dtype=np.float32)
-    batch_tst = [torch.from_numpy(np.asarray([patch])).cuda()
-                 for patch in batch_tst]
-    # batch_tst = torch.from_numpy(batch_tst).cuda()
-    # labels_tst = torch.from_numpy(labels_tst).cuda()
+    batch_tst = torch.from_numpy(batch_tst)
+    labels_tst = torch.from_numpy(labels_tst)
+    if args.gpu:
+        batch_tst = batch_tst.cuda()
+        labels_tst = labels_tst.cuda()
 
     try:
         cost = []
         for t in range(500):
 
-            X, y = queue_feed.get()
+            X, y = load_patches(_EXAMPLE_SUBJECT)
+            # X, y = queue_feed.get()
             if args.gpu:
                 X = X.cuda()
                 y = y.cuda()
@@ -72,19 +76,24 @@ if __name__ == "__main__":
             loss.backward()
             optimizer.step()
             print("[Iteration {}] Testing.".format(t))
-            tst_pred = np.array([unet(patch).data for patch in batch_tst])
-            tst_pred = stitch_image(tst_pred, img_shape)
-            plot_segmentation(X_tst, y_tst, tst_pred)
+            # tst_pred = np.array(unet(batch_tst).data)
+            tst_pred = np.array(unet(X).data)
+            tst_pred = np.argmax(tst_pred, axis=1)
+            # tst_pred = stitch_image(tst_pred, img_shape)
+            # fig = plot_segmentation(X_tst, tst_pred, y_tst)
+            fig = plot_patch_prediction(
+                np.array(X.data)[0, 0], np.array(y.data)[0], tst_pred[0], z=30)
+            fig.savefig('prediction_iteration_{}.png'.format(t))
             print("[Iteration {}] Finished.".format(t))
 
     finally:
         print("Stopping the batche_feeders")
-        stop_event.set()
-        for p in batch_feeder:
-            try:
-                # MAke some room in the queue if it is saturated
-                queue_feed.get()
-            except Exception:
-                pass
-        for p in batch_feeder:
-            p.join()
+        # stop_event.set()
+        # for p in batch_feeder:
+        #     try:
+        #         # MAke some room in the queue if it is saturated
+        #         queue_feed.get()
+        #     except Exception:
+        #         pass
+        # for p in batch_feeder:
+        #     p.join()